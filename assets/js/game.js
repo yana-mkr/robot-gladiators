aa254ae105f9e27f61045294749d291e527e0a1e
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-03-24 21:58:59 -0600

    first structure of game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log(10 + 10);
+console.log ("our robot's name is " + playerName);
+// this creates a function named "fight"
+function fight () {
+    window.alert("The fight has begun");
+}
+
+//fight();
