056b7e7fa0d11ae485f584fef05fe6485124bda5
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-03-24 22:18:06 -0600

    initial blows

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,34 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(10 + 10);
-console.log ("our robot's name is " + playerName);
-// this creates a function named "fight"
-function fight () {
-    window.alert("The fight has begun");
+console.log(playerName, playerHealth, playerAttack);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+if (playerHealth <=0) {
+    window.alert(playerName + "has died!");
+} else {
+    window.alert(playerName + " still has " + playerHealth + " left.");
 }
 
-//fight();
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+} else {
+    window.alert(enemyName + " still has " + enemyHealth + " left.");
+}
+
+};
+
+fight();
