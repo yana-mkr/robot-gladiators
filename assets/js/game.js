52b9654a8d99434b4b94f2f165d0e4a7c3886516
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-03-26 16:54:22 -0600

    getPlayerName function loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,7 +80,6 @@ var randomNumber = function(min, max) {
       if (playerInfo.health > 0) {
         // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-  
         // pick new enemy to fight based on the index of the enemyInfo array
         var pickedEnemyObj = enemyInfo[i];
   
@@ -170,8 +169,20 @@ var randomNumber = function(min, max) {
   /* GAME INFORMATION / VARIABLES */
   
   // player information
+  var getPlayerName = function() {
+    var name = "";
+
+ while ( name === "" || name === null) {
+   name = prompt("What is your robot's name?")
+ }
+
+  console.log("Your robot's name is " + name);
+  return name;
+  }
+  
+
   var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
