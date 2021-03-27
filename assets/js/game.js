1dc4460ae6f73793c232004aace050fdc342ea84
Yana Mkrtchyan <yana@Yanas-MacBook-Pro.local>
2021-03-26 22:13:06 -0600

    changed shop actions to numbers

--- assets/js/game.js
+++ assets/js/game.js
@@ -151,32 +151,26 @@ var endGame = function() {
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
+    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 to LEAVE.'
   );
 
   // use switch case to carry out action
-  switch (shopOptionPrompt) {
-    case 'REFILL':
-    case 'refill':
-      playerInfo.refillHealth();
-      break;
-    case 'UPGRADE':
-    case 'upgrade':
+  shopOptionPrompt = parseInt(shopOptionPrompt);
+ switch (shopOptionPrompt) {
+   case 1:
+     playerInfo.refillHealth();
+     break;
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case 'LEAVE':
-    case 'leave':
-      window.alert('Leaving the store.');
-
-      // do nothing, so function will end
+    case 3:
+      window.alert("Leave the store.");
       break;
-    default:
-      window.alert('You did not pick a valid option. Try again.');
-
-      // call shop() again to force player to pick a valid option
-      shop();
-      break;
-  }
+      default:
+        window.alert("You did not pick a valid option. Try again.");
+        shop();
+        break;
+ } 
 };
 
 /* END GAME FUNCTIONS */
